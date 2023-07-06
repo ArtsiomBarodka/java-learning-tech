d1a0aca777dcd15e7c6f71b556378b7b2cc8884d
Artsiom_Barodka <artsiombarodka@gmail.com>
2023-07-06 18:09:12 +0300

    Module: Redis - fit and finished

--- redis/src/main/java/com/epam/app/service/RateLimitRuleServiceImpl.java
+++ redis/src/main/java/com/epam/app/service/RateLimitRuleServiceImpl.java
@@ -3,20 +3,30 @@ package com.epam.app.service;
 import com.epam.app.config.RulesConfig;
 import com.epam.app.model.dto.RateLimitRule;
 import com.epam.app.model.request.DescriptorListRequest;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class RateLimitRuleServiceImpl implements RateLimitRuleService {
     private final RulesConfig rulesConfig;
+    private List<RateLimitRule> rules;
+
+    @PostConstruct
+    void init() {
+        rules = rulesConfig.getRules();
+        rules.sort(Comparator.comparingInt(this::getSpecificityLevel));
+    }
 
     @Override
     public @NonNull Optional<RateLimitRule> findMatchingRule(@NonNull DescriptorListRequest.DescriptorRequest descriptor) {
-        for (var rule : rulesConfig.getRules()) {
+        for (var rule : rules) {
             if (matchesRule(rule, descriptor)) {
                 return Optional.of(rule);
             }
@@ -36,6 +46,20 @@ public class RateLimitRuleServiceImpl implements RateLimitRuleService {
                 (descriptor.getRequestType() != null && descriptor.getRequestType().equals(rule.getRequestType()));
 
         return matchesAccountId && matchesClientIp && matchesRequestType;
+    }
+
+    private int getSpecificityLevel(RateLimitRule rule) {
+        int level = 0;
+        if (rule.getAccountId() != null) {
+            level++;
+        }
+        if (rule.getClientIp() != null) {
+            level++;
+        }
+        if (rule.getRequestType() != null) {
+            level++;
+        }
+        return level;
     }
 
 }
